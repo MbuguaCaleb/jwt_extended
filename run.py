7e88eab2cda227154b9952777dbaec988074ea2d
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-02-12 14:05:27 +0300

    [ft:]adds config for blacklisted tokens

--- run.py
+++ run.py
@@ -6,17 +6,31 @@ from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
-"""sqlalchemy configs"""
-app.config['JWT_SECRET_KEY'] = 'Jwt-secret-string'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-app.config['SECRET_KEY']='some-secret-string'
 
 """Initializations by passing classinstaces to app"""
 
 db=SQLAlchemy(app)
 api = Api(app)
 jwt=JWTManager(app)
+
+
+"""sqlalchemy configs"""
+app.config['JWT_SECRET_KEY'] = 'Jwt-secret-string'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SECRET_KEY']='some-secret-string'
+
+
+"""enabling blacklisting in configuration"""
+app.config['JWT_BLACKLIST_ENABLED']=True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS']=['access','refresh']
+
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti=decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
 
 
 @app.before_first_request
