af686149d1f75dc6b60420f7703aced121f1d779
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-02-12 14:03:57 +0300

    [ft:]initializez adds revoked token model

--- models.py
+++ models.py
@@ -41,7 +41,6 @@ class UserModel(db.Model):
         except:
             return{'message':'Alas!something went wrong!!!'}
 
-
     @staticmethod
     def generate_hash(password):
         return sha256.hash(password)
@@ -50,5 +49,24 @@ class UserModel(db.Model):
     def verify_hash(password, hash):
         return sha256.verify(password, hash)
 
+    
+class RevokedTokenModel(db.Model):
+    __tablename__='revoked_tokens'
 
-        
+    id=db.Column(db.Integer,primary_key=True)
+    jti = db.Column(db.String(120))
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    
+    @classmethod
+    def is_jti_blacklisted(cls,jti):
+        query = cls.query.filter_by(jti=jti).first()
+        return bool(query)
+
+
+    
+
+    
